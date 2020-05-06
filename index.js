bd7ffd0d96e269ef4b4af78736c7390637d01f35
fjhansen <58237856+fjhansen@users.noreply.github.com>
2020-05-06 15:46:48 -0700

    next

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code away!
+const express = require("express"); 
+const postsRouter = require("./posts/postRouter")
+
+const server = express();
+const port = 1337;
+
+// global middleware
+server.use(express.json());
+
+server.use(function(req, res, next) {
+  const today = new Date().toISOString(); // YYYY-MM-DD mdn javascript date methods
+  console.log(`[${today}] This is a request with string interpolation!`);
+  next();
+})
+server.use('/api/users', postsRouter);
+
+server.get('/', (req, res) => {
+  res.send('❤ ❤ ❤ L I V E ❤ ❤ ❤')
+})
+
+server.listen(port, () => {
+  console.log(`\n ❤ ❤ ❤	 LIVE  ON  ${port} ❤ ❤ ❤	`)
+})
